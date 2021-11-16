41b9079d42d09a250b864f8d3058bfe4e854590e
IanGarciaUnam <iangarcia@ciencias.unam.mx>
2021-11-16 13:08:30 -0600

    covergecast | in progress

--- Practica2/Tree.ex
+++ Practica2/Tree.ex
@@ -9,8 +9,8 @@ defmodule Tree do
   defp loop() do
     receive do
       {:broadcast, tree, i, caller} -> broadcast_aux(tree,i,caller)
-      {:convergecast, tree, i, caller} -> :ok #aquí puede morir el diccionario de
-                                           # procesos o no
+      {:convergecast, tree, i, caller} -> convergecast_per_node(tree,i,caller, []) #aquí puede morir el diccionario de
+                                         # procesos o no
     end
   end
 
@@ -69,12 +69,58 @@ defmodule Tree do
     #Aquí va su código.
     #Paso 1, situarme en las hojas
 
-    hojas=div((n-1),2)
-    Enum.each((hojas..(n)), fn x ->send(tree[x], {:convergecast,tree,x, self()})end)
-    results=receive do
+    hojas=(ceil n / 2)
+    Enum.each((n-hojas)..(n-1),fn x -> send(tree[x], {:convergecast,tree,x, self()}) end)
+    recibidos = receive do
       w->w
     end
-    results
+    recibidos
+  end
+
+  def convergecast_per_node(tree, x, caller, registro)do
+    padre=padre(x)
+    hijos=verifica_hijos(tree,x)
+    registro++hijos
+    num_hijos=Enum.count(hijos)
+
+    cond do
+      x==0 ->#soy raíz
+        send(caller, {:ok,0})
+      num_hijos==0->#Es la hoja
+        send(tree[padre], {:convergecast, tree, padre, caller})
+      num_hijos==Enum.count(registro)->
+        send(tree[padre], {:convergecast, tree, padre, caller})
+    end
+    registro
+  end
+
+  defp reinicia_nodos(tree)do
+    Enum.each(Map.values(tree), fn proceso-> send(proceso, :reset)end)
+  end
+
+
+  def verifica_hijos(tree,x) do
+    l=[]
+    if Map.has_key?(tree,derecho(x))do
+      Enum.concat(l,derecho(x))
+    end
+    if Map.has_key?(tree,izquierdo(x))do
+      Enum.concat(l,izquierdo(x))
+    end
+    l
+
+  end
+
+  def izquierdo n do
+    2*n+1
+  end
+
+  def derecho n do
+    2*n+2
+  end
+
+  def padre n do
+    floor((n-1)/2)
   end
 
 
