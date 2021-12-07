c905d7ff888973ec0c005d7f90639cd116669f8b
armando122 <armandoramirezg@ciencias.unam.mx>
2021-12-06 18:21:50 -0600

    Indexando elementos

--- Practica3/Consensus.ex
+++ Practica3/Consensus.ex
@@ -10,7 +10,25 @@ defmodule Consensus do
     Enum.map(1..n, fn _ ->
       spawn(fn -> loop(:start, 0, :rand.uniform(10)) end)
     end)
+
+    #Función para indexar los hilos
+    indexa(Enum.map(1..n, fn _ ->
+      spawn(fn -> loop(:start, 0, :rand.uniform(10)) end)
+    end), %{}, 0)
+
     #Agregar código es valido
+  end
+
+  # Función auxiliar indexa para indexar los procesos
+  # en un diccionario.
+  defp indexa([], procesos, _) do
+    procesos
+  end
+
+  # Función auxiliar indexa para indexar los procesos
+  # en un diccionario. (Sobrecarga de método)
+  defp indexa([pid | l], procesos, pos) do
+    indexa(l,Map.put(procesos, pos, pid), (pos+1))
   end
 
   defp loop(state, value, miss_prob) do
@@ -19,20 +37,23 @@ defmodule Consensus do
     end
     receive do
       {:get_value, caller} ->
-	send(caller, value)
+        send(caller, value)
 	#Aqui se pueden definir mas mensajes
     after
       1000 -> :ok #Analizar por qué esto esta aqui
     end
     case state do
       :start ->
-	chosen = :rand.uniform(10000)
-	if(rem(chosen, miss_prob) == 0) do
-	  loop(:fail, chosen, miss_prob)
-	else
-	  loop(:active, chosen, miss_prob)
-	end
+        chosen = :rand.uniform(10000)
+
+        if(rem(chosen, miss_prob) == 0) do
+          loop(:fail, chosen, miss_prob)
+        else
+          loop(:active, chosen, miss_prob)
+        end
+
       :fail -> loop(:fail, value, miss_prob)
+
       :active -> :ok #Aquí va su código.
     end
   end
