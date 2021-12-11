d29dd5fc3d022529891d1c17532878377ad293f1
armando122 <armandoramirezg@ciencias.unam.mx>
2021-12-10 22:11:49 -0600

    Parece que todo funciona bien

--- Practica3/Consensus.ex
+++ Practica3/Consensus.ex
@@ -8,51 +8,61 @@ defmodule Consensus do
     #todos los hilos tengan el mismo número, el cual va a ser enviado vía un
     #mensaje al hilo principal.
     Enum.map(1..n, fn _ ->
-      spawn(fn -> loop(:start, 0, :rand.uniform(10)) end)
+      spawn(fn -> loop(:start, 0, :rand.uniform(10),[]) end)
     end)
 
   end
 
-  # Función loop que recibe como último parametro
-  # una lista correspondiente a los vecinos del proceso.
-  defp loop(state, value, miss_prob) do
+  #Función loop que recibe el estado del proceso, el valor
+  # elegido, la probabilidad de fallar y la lista de vecinos.
+  defp loop(state, value, miss_prob,vecinos) do
     #inicia código inamovible.
     if(state == :fail) do
-      loop(state, value, miss_prob)
+      loop(state, value, miss_prob,vecinos)
     end
 
     # Termina código inamovible.
     receive do
       {:get_value, caller} ->
-	send(caller, value) #No modificar.
+        send(caller, value) #No modificar.
 
-      {}
+      {:vecinos,list} -> loop(state,value,miss_prob,list)
+
+      {:add,val} ->
+        if val<value do
+          loop(state,val,miss_prob,vecinos)
+        else
+          loop(state,value,miss_prob,vecinos)
+        end
+
     after
       1000 -> :ok #Aquí analizar porqué está esto aquí.
 
     end
+
     case state do
       :start ->
         chosen = :rand.uniform(10000)
 
         if(rem(chosen, miss_prob) == 0) do
-          loop(:fail, chosen, miss_prob)
+          loop(:fail, chosen, miss_prob,vecinos)
         else
-          loop(:active, chosen, miss_prob)
+          Process.sleep(5000)
+          loop(:active, chosen, miss_prob,vecinos)
         end
 
-      :fail -> loop(:fail, value, miss_prob)
+      :fail -> loop(:fail, value, miss_prob,vecinos)
 
       #Envío de su propuesta a los demás procesos
       :active ->
         for pid <- vecinos do
           send(pid,{:add,value})
-          loop(:wait,value,miss_prob)
         end
+        loop(:wait,value,miss_prob,vecinos)
 
-      #Mensaje para almacenar las propuestas recibidas.
-      #Y tomar una decisión después de segundos.
-      :wait -> :ok
+      # Mensaje para que el proceso se siga ejecutando
+      # mientras recibe las propuestas.
+      :wait -> loop(state,value,miss_prob,vecinos)
 
     end
   end
@@ -60,7 +70,7 @@ defmodule Consensus do
   def consensus(processes) do
     # Establecemos la lista de vecinos de cada proceso.
     for v <- processes do
-      send(v, {:add, List.delete(processes,v)})
+      send(v, {:vecinos, List.delete(processes,v)})
     end
 
     Process.sleep(10000)
@@ -69,9 +79,9 @@ defmodule Consensus do
     for pid <- processes do
       send(pid, {:get_value, self()})
       receive do
-        value -> IO.puts(inspect(p), " eligió: ", inspect(value))
+        value -> IO.puts(inspect(pid)<> " eligió: " <> inspect(value))
       after
-        100 -> IO.puts(inspect(p), " fallido" )
+        100 -> IO.puts(inspect(pid)<> " fallido" )
       end
     end
 
