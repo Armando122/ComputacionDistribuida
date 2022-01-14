a7000b0b03c42f3affc0d52f41a00a70ebc3033c
armando122 <armandoramirezg@ciencias.unam.mx>
2022-01-13 18:14:50 -0600

    Agregando comentarios

--- Practica4/SlidingWindow.ex
+++ Practica4/SlidingWindow.ex
@@ -1,13 +1,20 @@
+#Modulo encargado de generar un paquete de longitud n.
+# Que contiene números elegidos añeatoriamente entre 0 y 1.
 defmodule GeneratePackage do
 
   def new(n) do
     Enum.map(1..n, fn x -> num = :rand.uniform(2)-1 end)
   end
-  
+
 end
 
+
+#Modulo para ejecutar el algoritmo de la ventana deslizante.
 defmodule SlidingWindow do
 
+  # Función encargada de crear un paquete de longitud n,
+  # el tamaño de la ventana que será a lo más de longitud n/2
+  # y los procesos sender y receiver.
   def new(n) do
     package = GeneratePackage.new(n)
     k = :rand.uniform(div(n, 2))
@@ -19,8 +26,9 @@ defmodule SlidingWindow do
     :ok
   end
 
+  # Función encargada de iniciar la transmisión.
   def recvr_loop(sender) do
     :ok
   end
-  
+
 end
