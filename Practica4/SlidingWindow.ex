d7f4eae3b5ca73b61fb9132353c3d2bf878aadbf
Ian Garcia <iangarcia@2806-107e-0017-05c2-4410-68f1-000b-148f.ipv6.infinitum.net.mx>
2022-01-22 23:30:16 -0600

    1st version- not working

--- Practica4/SlidingWindow.ex
+++ Practica4/SlidingWindow.ex
@@ -11,16 +11,40 @@ defmodule SlidingWindow do
   def new(n) do
     package = GeneratePackage.new(n)
     k = :rand.uniform(div(n, 2))
-    sender = spawn(fn -> sender_loop(package, n, k) end)
-    recvr = spawn(fn -> recvr_loop(sender) end)
+    #sender = spawn(fn -> sender_loop(package, n, k) end)
+    #recvr = spawn(fn -> recvr_loop(sender) end)
   end
 
+
+
+  
   def sender_loop(package, n, k) do
-    :ok
+    axl_sender(package, 0, n ,k)
+  end
+
+  def axl_sender(package, i, n, k) do
+    if i>=n do
+      package
+    end
+    send(recv_loop, {i, Enum.at(package, i)})
+
+    receive do
+      {w,v} -> if w==i do
+                axl_sender(package,i+1,n,k)
+               end
+    end
+
   end
 
   def recvr_loop(sender) do
-    :ok
+    out=[]
+    m={:a, 2}
+    receive do
+      {x , y} -> out++[y]
+      send(axl_sender, {x,y})
+      after 
+        5000 -> :timeout 
+    end
   end
   
 end
